b875c1fa174d6c699839eb901badb367b92738cb
kama008 <komronbeknasibjonov2008@gmail.com>
2023-12-25 16:20:01 +0500

    js_dz2.1

---
+++ app.js
@@ -0,0 +1,76 @@
+var time = +prompt('Введите время')
+
+switch(time) {
+    case 0:
+    alert('12 Часа ночи' )
+    break
+    case 1:
+       alert('1 Часа ночи')
+       break
+    case 2:
+       alert('2 Часа ночи')
+       break
+    case 3:
+       alert('3 Часа ночи')
+       break
+    case 4:
+       alert('4 Часа ночи')
+       break
+    case 5:
+       alert('5 Часа ночи')
+       break
+    case 6:
+       alert('6 Часа ночи')
+       break
+    case 7:
+       alert('7 Часов утра')
+       break
+    case 8:
+       alert('8 Часов утра')
+       break
+    case 9:
+       alert('9 Часов утра')
+       break
+    case 10:
+       alert('10 Часов утра')
+       break
+    case 11:
+       alert('11 Часов дня')
+       break
+    case 12:
+       alert('12 Часов дня')
+       break
+    case 13:
+       alert('1 Часов дня')
+       break
+    case 14:
+       alert('2 Часов дня')
+       break
+    case 15:
+       alert('3 Часов дня')
+       break
+    case 16:
+       alert('4 Часов дня')
+       break
+    case 17:
+       alert('5 Часов дня')
+       break
+    case 18:
+       alert('6 Часов вечера')   
+       break
+    case 19:
+       alert('7 Часов вечера')   
+       break
+    case 20:
+       alert('8 Часов вечера')   
+       break
+    case 21:
+       alert('9 Часов вечера')   
+       break
+    case 22:
+       alert('10 Часов вечера')   
+       break
+    case 23:
+       alert('11 Часов вечера')   
+       break
+}
